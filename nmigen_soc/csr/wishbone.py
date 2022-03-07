0229e231f5f671236aee69ad677b2ea946847cec
Luke Kenneth Casson Leighton <lkcl@lkcl.net>
2022-03-07 11:59:15 +0000

    when WB 4 "pipeline" mode requests are sent, cyc/stb is *not* guaranteed to both be held HI: only cyc.  therefore use the initial cyc/stb to activate the CSR read/write, and *carry on* doing so even after stb is lowered.

--- nmigen_soc/csr/wishbone.py
+++ nmigen_soc/csr/wishbone.py
@@ -37,22 +37,26 @@ class WishboneCSRBridge(Elaboratable):
     """
     def __init__(self, csr_bus, *, data_width=None, name=None):
         if not isinstance(csr_bus, CSRInterface):
-            raise ValueError("CSR bus must be an instance of CSRInterface, not {!r}"
+            raise ValueError("CSR bus must be an instance "
+                             "of CSRInterface, not {!r}"
                              .format(csr_bus))
         if csr_bus.data_width not in (8, 16, 32, 64):
-            raise ValueError("CSR bus data width must be one of 8, 16, 32, 64, not {!r}"
+            raise ValueError("CSR bus data width must be "
+                             "one of 8, 16, 32, 64, not {!r}"
                              .format(csr_bus.data_width))
         if data_width is None:
             data_width = csr_bus.data_width
 
         self.csr_bus = csr_bus
         self.wb_bus  = WishboneInterface(
-            addr_width=max(0, csr_bus.addr_width - log2_int(data_width // csr_bus.data_width)),
+            addr_width=max(0, csr_bus.addr_width - 
+                              log2_int(data_width // csr_bus.data_width)),
             data_width=data_width,
             granularity=csr_bus.data_width,
             name="wb")
 
-        wb_map = MemoryMap(addr_width=csr_bus.addr_width, data_width=csr_bus.data_width,
+        wb_map = MemoryMap(addr_width=csr_bus.addr_width,
+                           data_width=csr_bus.data_width,
                            name=name)
         # Since granularity of the Wishbone interface matches the data width of the CSR bus,
         # no width conversion is performed, even if the Wishbone data width is greater.
@@ -64,37 +68,54 @@ class WishboneCSRBridge(Elaboratable):
         wb_bus  = self.wb_bus
 
         m = Module()
+        comb, sync = m.d.comb, m.d.sync
+        slen = len(wb_bus.sel)
 
         # cycle through at the granularity of the Wisbone Bus, updating the CSR
         # note: cycle is up to 1 more than the wb.sel granularity.
         # use cycle to construct the CSR bus address.
-        cycle = Signal(range(len(wb_bus.sel) + 1))
-        m.d.comb += csr_bus.addr.eq(Cat(cycle[:log2_int(len(wb_bus.sel))], wb_bus.adr))
+        cycle = Signal(range(slen + 1))
+        comb += csr_bus.addr.eq(Cat(cycle[:log2_int(slen)], wb_bus.adr))
 
-        with m.If(wb_bus.cyc & wb_bus.stb):
+        # when cyc/stb are first raised, cycle starts progressing.
+        # however, on WB4 pipeline-mode requests we cannot rely on stb
+        # being held hi indefinitely (like it is in WB3 "classic").
+        # use cycle being non-zero to continue sending CSR requests
+        is_in_progress = Signal()
+        comb += is_in_progress.eq(cycle != 0)
+
+        # WB 4 pipeline mode w/stall
+        if hasattr(wb_bus, "stall"):
+            comb += wb_bus.stall.eq(~ack)
+
+        with m.If(wb_bus.cyc & (wb_bus.stb | is_in_progress)):
+            # cover up to len(sel)+1 cases, last one does the "ack"
             with m.Switch(cycle):
                 def segment(index):
-                    return slice(index * wb_bus.granularity, (index + 1) * wb_bus.granularity)
+                    return slice(wb_bus.granularity * index,
+                                 wb_bus.granularity * (index + 1))
 
                 # cycle between 0..len(wb.sel)-1
                 for index, sel_index in enumerate(wb_bus.sel):
+                    prev_seg = segment(index - 1)
+                    seg = segment(index)
                     with m.Case(index):
                         if index > 0:
-                            # CSR reads are registered, and we need to re-register them.
-                            m.d.sync += wb_bus.dat_r[segment(index - 1)].eq(csr_bus.r_data)
-                        m.d.comb += csr_bus.r_stb.eq(sel_index & ~wb_bus.we)
-                        m.d.comb += csr_bus.w_data.eq(wb_bus.dat_w[segment(index)])
-                        m.d.comb += csr_bus.w_stb.eq(sel_index & wb_bus.we)
-                        m.d.sync += cycle.eq(index + 1)
+                            # CSR reads registered: need to re-register them.
+                            sync += wb_bus.dat_r[prev_seg].eq(csr_bus.r_data)
+                        comb += csr_bus.r_stb.eq(sel_index & ~wb_bus.we)
+                        comb += csr_bus.w_data.eq(wb_bus.dat_w[seg])
+                        comb += csr_bus.w_stb.eq(sel_index & wb_bus.we)
+                        sync += cycle.eq(index + 1)
 
                 # cycle is len(wb.sel). use this to send an ack
                 with m.Default():
-                    m.d.sync += wb_bus.dat_r[segment(index)].eq(csr_bus.r_data)
-                    m.d.sync += wb_bus.ack.eq(1)
+                    sync += wb_bus.dat_r[seg].eq(csr_bus.r_data)
+                    sync += wb_bus.ack.eq(1)
 
         # one clock later, clear ack and reset cycle back to zero
         with m.If(wb_bus.ack):
-            m.d.sync += cycle.eq(0)
-            m.d.sync += wb_bus.ack.eq(0)
+            sync += cycle.eq(0)
+            sync += wb_bus.ack.eq(0)
 
         return m
